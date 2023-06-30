5ca7885569c629cd70a07e4c3d62e93f786b6050
klmwook <klmwook72@gmail.com>
2023-06-30 12:46:50 +0900

    react portfolio 기본 틀 추가

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import { Route, Switch } from 'react-router-dom';
+
+function App() {
+	return <h1>Hello</h1>;
+}
+
+export default App;
---
+++ src/components/main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../common/Header';
+
+function Main() {
+	return (
+		<main>
+			<Header />
+		</main>
+	);
+}
+
+export default Main;
