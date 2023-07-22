459898528bd4afd90f45ef1dc18f4b4e9260e46d
klmwook <klmwook72@gmail.com>
2023-07-23 00:54:57 +0900

    gallery subpage 기능 개발 완료

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,4 +1,5 @@
 import { forwardRef, useState, useImperativeHandle } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 //forwordRef - 자식 컴포넌트 요소를 호출하는 부모 컴포넌트에 역으로 참조해서 전달
 const Modal = forwardRef((props, ref) => {
@@ -11,14 +12,29 @@ const Modal = forwardRef((props, ref) => {
 
 	return (
 		<>
-			{Open && (
-				<aside className='pop on' ref={ref}>
-					<div className='con'>{props.children}</div>
-					<span className='close' onClick={() => setOpen(false)}>
-						close
-					</span>
-				</aside>
-			)}
+			<AnimatePresence>
+				{Open && (
+					// 모션은 걸고 싶은 컴포넌트에 motion.지정 initial(모션시작), animate(모션완료), exit(사라지는 모션) 속성 지정
+					//x(가로축), y(세로축), rotate(회전), scale(확대축소)
+					<motion.aside
+						className='modal'
+						initial={{ opacity: 0, x: '100%' }}
+						animate={{ opacity: 1, x: '0%', transition: { duration: 0.5 } }}
+						exit={{ opacity: 0, scale: 0, transition: { duration: 0.5, delay: 0.5 } }}
+					>
+						<motion.div className='con' initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.5 } }} exit={{ opacity: 0, transition: { delay: 0 } }}>
+							{props.children}
+						</motion.div>
+						<motion.span
+							className='close'
+							onClick={() => setOpen(false)}
+							initial={{ opacity: 0, y: 100 }}
+							animate={{ opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.5 } }}
+							exit={{ opacity: 0, x: -100, transition: { duration: 0.5, delay: 0 } }}
+						></motion.span>
+					</motion.aside>
+				)}
+			</AnimatePresence>
 		</>
 	);
 });
--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,20 +1,22 @@
 import React, { useEffect, useRef, useState } from 'react';
+import Masonry from 'react-masonry-component';
 import { useSelector, useDispatch } from 'react-redux/';
+import { fetchFlickr } from '../../redux/flickrSlice';
 import Layout from '../common/Layout';
-
-/* 
-	데이터 가져오기 위한 설정 부터 하고
-	reduxjs toolkit 설치 : npm i @reduxjs/toolkit react-redux
-	Masonry 설치
-*/
+import Modal from '../common/Modal';
 
 function Gallery() {
+	const dispatch = useDispatch();
+	const openModal = useRef(null);
+	const [Index, setIndex] = useState(0);
 	const Items = useSelector((store) => store.flickr.data);
 	const wrap = useRef(null);
 	const Loading = useRef(null);
 	const searchInput = useRef('');
 	const enableEvent = useRef(true);
 	const counter = useRef(0);
+	const firstLoaded = useRef(true);
+	const isUser = useRef(true);
 
 	const resetGallery = (e) => {
 		enableEvent.current = false;
@@ -22,32 +24,92 @@ function Gallery() {
 		wrap.current.classList.remove('on');
 	};
 
+	//인기있는 갤러리 모음
+	const showInterest = (e) => {
+		if (!enableEvent.current) return;
+		resetGallery(e);
+		dispatch(fetchFlickr({ type: 'interest' }));
+		isUser.current = false;
+	};
+
+	//내 갤러리 모음
+	const showMine = (e) => {
+		if (!enableEvent.current) return;
+		resetGallery(e);
+		dispatch(fetchFlickr({ type: 'user', user: '198489363@N07' }));
+		isUser.current = false;
+	};
+
+	const showSearch = (e) => {
+		const tag = searchInput.current.value.trim();
+		if (tag === '') return alert('검색어를 입력하세요.');
+		if (!enableEvent.current) return;
+
+		resetGallery(e);
+		dispatch(fetchFlickr({ type: 'search', tags: tag }));
+		searchInput.current.value = '';
+	};
+
 	useEffect(() => {
-		//Loading.current.Open();
-		enableEvent.current = true;
-		wrap.current.classList.add('on');
-	}, [wrap]);
+		Loading.current.Open();
+		counter.current = 0;
+
+		if (Items.length === 0 && !firstLoaded.current) {
+			Loading.current.Close();
+			wrap.current.classList.add('on');
+			dispatch(fetchFlickr({ type: 'user', user: '198489363@N07' }));
+			enableEvent.current = true;
+			return alert('이미지 결과값이 없습니다.');
+		}
+
+		console.log(Items);
+
+		firstLoaded.current = false;
+		const imgs = wrap.current.querySelectorAll('img');
+
+		imgs.forEach((img) => {
+			img.onload = () => {
+				++counter.current;
+
+				if (counter.current === imgs.length - 2) {
+					Loading.current.Close();
+					wrap.current.classList.add('on');
+					enableEvent.current = true;
+				}
+			};
+		});
+	}, [Items, dispatch]);
 
 	return (
 		<>
 			<Layout id={'Sub_Gallery'} title={'Gallery'} ref={Loading}>
 				<div className='Search_Area'>
 					<div className='input_Area'>
-						<input type='text' required ref={searchInput} />
+						<input type='text' required ref={searchInput} onKeyPress={(e) => e.key === 'Enter' && showSearch(e)} />
 						<label>검색어 입력</label>
-						<button>Search</button>
+						<button onClick={showSearch}>Search</button>
 					</div>
 					<div className='button_Area'>
-						<button className='int'>Interest</button>
-						<button className='my'>My</button>
+						<button className='int' onClick={showInterest}>
+							Interest
+						</button>
+						<button className='my' onClick={showMine}>
+							My
+						</button>
 					</div>
 				</div>
 				<div className='contents' ref={wrap}>
-					<ul>
+					<Masonry elementType={'ul'} options={{ transitionDuration: '0.5s' }}>
 						{Items.map((item, idx) => {
 							return (
 								<li key={idx}>
-									<div className='Img_Area'>
+									<div
+										className='Img_Area'
+										onClick={() => {
+											openModal.current.Open();
+											setIndex(idx);
+										}}
+									>
 										<img
 											className='thumb'
 											src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
@@ -63,14 +125,28 @@ function Gallery() {
 											alt={item.owner}
 											onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
 										/>
-										<span className='userid'>${item.owner}</span>
+										<span
+											className='userid'
+											onClick={(e) => {
+												if (isUser.current) return;
+												isUser.current = true;
+												wrap.current.classList.remove('on');
+												dispatch(fetchFlickr({ type: 'user', user: e.target.innerText }));
+											}}
+										>
+											{item.owner}
+										</span>
 									</div>
 								</li>
 							);
 						})}
-					</ul>
+					</Masonry>
 				</div>
 			</Layout>
+
+			<Modal ref={openModal}>
+				<img src={`https://live.staticflickr.com/${Items[Index]?.server}/${Items[Index]?.id}_${Items[Index]?.secret}_b.jpg`} alt={Items[Index]?.title} />
+			</Modal>
 		</>
 	);
 }
