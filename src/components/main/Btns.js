73b05cda742f75df8a45762bf4af15a7be8c9dc6
klmwook <klmwook72@gmail.com>
2023-08-09 11:38:21 +0900

    메인 스크롤 기능 동작 수정

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -14,6 +14,7 @@ function Btns({ setScrolled, setPos }) {
 		for (const sec of secs) pos.current.push(sec.offsetTop);
 		setNum(pos.current.length);
 		setPos(pos.current);
+		console.log(pos.current);
 	}, []);
 
 	const activation = useCallback(() => {
