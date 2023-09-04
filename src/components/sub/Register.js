2c3a41449b63794bbf2d8ee789d2dc65025ce8a7
klmwook <klmwook72@gmail.com>
2023-09-04 10:10:19 +0900

    Debounce 기능 Register 페이지에 추가

--- src/components/sub/Register.js
+++ src/components/sub/Register.js
@@ -105,7 +105,7 @@ function Register() {
 	useEffect(() => {
 		const len = Object.keys(Err).length;
 		if (len === 0 && Submit) {
-			alert('모든 인증을 통과했습니다.');
+			alert('모든 인증을 통과했습니다. ');
 			//history.push('/');
 			resetForm();
 		}
